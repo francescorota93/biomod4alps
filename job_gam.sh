5a5bb5675b2a5c524a5bbf896d7f7240db2b727d
GiulioGenova <giuliogenova89@gmail.com>
2020-04-11 17:11:16 +0000

    adding job files for every model

---
+++ job_gam.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J gam
+#SBATCH -N 1
+#SBATCH --partition=mem_0064
+#SBATCH --qos=normal_0064
+##SBATCH --account=lv71284
+#SBATCH --mail-user=giulio.genova@eurac.edu
+#SBATCH --mail-type=BEGIN,END
+##SBATCH --time-min=3
+
+module purge
+module load intel/18 intel-mkl/2018 R/3.6.2 gdal/2.4.1 proj/4.9.3
+
+Rscript $HOME/data/script/launch_run_models_cmd.R GAM 4
---
+++ job_rf_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J RF_test
+#SBATCH -N 1
+#SBATCH --partition=mem_0128
+#SBATCH --qos=devel_0128
+##SBATCH --account=lv71284
+#SBATCH --mail-user=giulio.genova@eurac.edu
+#SBATCH --mail-type=BEGIN,END
+##SBATCH --time-min=3
+
+module purge
+module load intel/18 intel-mkl/2018 R/3.6.2 gdal/2.4.1 proj/4.9.3
+
+Rscript $HOME/data/script/launch_run_models_cmd.R RF 4
