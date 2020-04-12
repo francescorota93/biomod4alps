cb39801622450c5c4b2636663b9e984d6fd49b89
GiulioGenova <giuliogenova89@gmail.com>
2020-04-12 22:14:00 +0000

    adding jobs for new launchers

---
+++ job_build_models_test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J build_models_test
+#SBATCH -N 1
+#SBATCH --partition=mem_0128
+#SBATCH --qos=devel_0128
+##SBATCH --account=lv71284
+#SBATCH --mail-user=giulio.genova@eurac.edu
+#SBATCH --mail-type=BEGIN,END
+##SBATCH --time-min=3
+
+module purge
+module load intel/18 intel-mkl/2018 R/3.6.2 gdal/2.4.1 proj/4.9.3
+
+Rscript $HOME/data/biomod4alps/launch_build_models_cmd.R all 15
---
+++ job_project_future.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -J project_current
+#SBATCH -N 1
+#SBATCH --partition=mem_0128
+#SBATCH --qos=normal_0128
+##SBATCH --account=lv71284
+#SBATCH --mail-user=giulio.genova@eurac.edu
+#SBATCH --mail-type=BEGIN,END
+##SBATCH --time-min=3
+
+module purge
+module load intel/18 intel-mkl/2018 R/3.6.2 gdal/2.4.1 proj/4.9.3
+
+Rscript $HOME/data/biomod4alps/launch_project_current_cmd.R all 20
