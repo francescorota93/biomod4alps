da4be78544f9c21498b0c455e7462ceec5400c55
Giulio Genova <giuliogenova89@gmail.com>
2020-04-13 12:19:49 +0200

    put 24 to job_project_future

--- job_project_future.sh
+++ job_project_future.sh
@@ -11,4 +11,4 @@
 module purge
 module load intel/18 intel-mkl/2018 R/3.6.2 gdal/2.4.1 proj/4.9.3
 
-Rscript $HOME/data/biomod4alps/launch_project_current_cmd.R all 20
+Rscript $HOME/data/biomod4alps/launch_project_current_cmd.R all 24
