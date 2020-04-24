97394a2c353f39257bb7527f57d71f21bf030ad7
Francesco Rota <francesco.rota@natec.unibz.it>
2020-04-24 14:36:25 +0000

    script per fare mask con geologia

--- mask_geo.R
+++ mask_geo.R
@@ -1,38 +1,24 @@
 ### for climate
 
-setwd("C:/Users/FRota/Desktop/topoclim/climate/")
+setwd("/data/models/")
 
 library(raster)
 
-d<-dir("models_past/", full.names=T)
-
-for(i in 1:length(d)){
-  setwd(d[i])
-  d2<-dir(".", pattern=glob2rx("*bin*.tif"))
+d<-dir("bin/", full.names=T)
+geo<- raster("geology/geology_endemic_dolo_all.tif")
+s <- read.table("endemic_dolo50.txt", head = TRUE, sep = "\t")
+species <- c(3,4) ### numero di specie c(1:8)
+for(i in species){
+  sp.names<-levels(factor(s[,1]))[i]
+  d2<-list.files("bin/", pattern = sp.names, full.names = TRUE)
   df <- stack(d2)
-  geo<- raster("../../../geology/geology_endemic_dolo.tif")
-  df_geo <- stack(mask(df, geo))
-  writeRaster(df_geo, filename=paste0(names(df_geo),"_geo.tif"), file.path(d[i]), bylayer=TRUE,format="GTiff", overwrite=TRUE)
-  setwd("..")
-  setwd("..")
+  t2 <- df@extent
+  cn  <- crop(geo, t2)
+  df_geo <- mask(df, cn)
+  if(!dir.exists("bin_geo")){
+    dir.create("bin_geo")
+  }
+  writeRaster(df_geo, filename=paste0("bin_geo/",names(df_geo),"_geo.tif"), bylayer=TRUE,format="GTiff", overwrite=TRUE)
 }
 
-
-### for topography
-setwd("C:/Users/FRota/Desktop/topoclim/topography/")
-
-library(raster)
-
-d<-dir("models/", full.names=T)
-
-for(i in 1:length(d)){
-  setwd(d[i])
-  d2<-dir(".", patter=".tif")
-  df <- stack(d2)
-  geo <- raster("../../../geology/geology_endemic_dolo.tif")
-  df_geo <- stack(mask(df, geo))
-  writeRaster(df_geo, filename=paste0(names(df_geo),"_geo.tif"), file.path(d[i]), bylayer=TRUE,format="GTiff", overwrite=TRUE)
-  setwd("..")
-  setwd("..")
-}
 
