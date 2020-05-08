4dbd1a2ffe99f56aa6fcbb3fa3f66b04b4e9e279
Francesco Rota <francesco.rota@natec.unibz.it>
2020-05-08 08:08:37 +0200

    script ok per specie 125678

--- job_project_current.sh
+++ job_project_current.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -J current
 #SBATCH -N 1
-#SBATCH --partition=mem_0064
-#SBATCH --qos=normal_0064
+#SBATCH --partition=mem_0128
+#SBATCH --qos=normal_0128
 ##SBATCH --account=lv71284
 ##SBATCH --mail-user=giulio.genova@eurac.edu
 #SBATCH --mail-user=francesco.rota@education.unibz.it
@@ -14,4 +14,4 @@ module load intel/18 intel-mkl/2018 R/3.6.2 gdal/2.4.1 proj/4.9.3
 
 ## Rscript $HOME/data/biomod4alps/launch_project_current.R all 15
 
-Rscript $HOME/data/biomod4alps/launch_project_current.R --cores 15 --workdir $HOME/data/ --scriptdir $HOME/data/biomod4alps -s 3 4 --models GLM GAM GBM RF CTA
+Rscript $HOME/data/biomod4alps/launch_project_current.R --cores 25  --workdir $HOME/data/ --scriptdir $HOME/data/biomod4alps -s 1 2 5 6 7 8 --models GLM GAM GBM RF CTA
