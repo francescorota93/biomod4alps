0945729f87b0437cdec6795a0f27fe8b47c6d2e7
Francesco Rota <francesco.rota@natec.unibz.it>
2020-05-08 08:09:16 +0200

    script ok per specie 125678

--- job_project_future.sh
+++ job_project_future.sh
@@ -14,4 +14,4 @@ module load intel/18 intel-mkl/2018 R/3.6.2 gdal/2.4.1 proj/4.9.3
 
 ## Rscript $HOME/data/biomod4alps/launch_project_future.R all 30
 
-Rscript $HOME/data/biomod4alps/launch_project_future.R -c 25  -w $HOME/data/ --scriptdir $HOME/data/biomod4alps -s 3 4 --models  GAM  RF 
+Rscript $HOME/data/biomod4alps/launch_project_future.R -c 25  -w $HOME/data/ --scriptdir $HOME/data/biomod4alps -s 1 2 5 6 7 8 --models GLM GBM GAM RF CTA 
