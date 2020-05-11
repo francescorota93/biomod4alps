6e0cc2c284f877d91b30532596dbe5130de953a7
Francesco Rota <francesco.rota@natec.unibz.it>
2020-05-11 12:44:03 +0200

    GBM for all species topography

--- job_project_topography.sh
+++ job_project_topography.sh
@@ -14,4 +14,4 @@ module load intel/18 intel-mkl/2018 R/3.6.2 gdal/2.4.1 proj/4.9.3
 
 ## Rscript $HOME/data/biomod4alps/launch_project_current.R all 15
 
-Rscript $HOME/data/biomod4alps/launch_project_current.R --cores 25  --workdir $HOME/data/ --scriptdir $HOME/data/biomod4alps -s 1 2 3 4 5 6 7 8 --models GLM GAM GBM RF CTA --outdir /models_topography --projection_name topo
+Rscript $HOME/data/biomod4alps/launch_project_current.R --cores 25  --workdir $HOME/data/ --scriptdir $HOME/data/biomod4alps -s 1 2 3 4 5 6 7 8 --models  GBM  --outdir /models_topography --projection_name topo
