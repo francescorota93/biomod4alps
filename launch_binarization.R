30108a73d77d5dbb364e5ad46b9a4e8479b37c6d
Francesco Rota <francesco.rota@natec.unibz.it>
2020-05-12 22:24:57 +0200

    setwd source script ok

--- launch_binarization.R
+++ launch_binarization.R
@@ -1,13 +1,13 @@
 setwd("/home/lv71284/frota/data/") ###setwd("/home/lv71284/frota/data/")
 library(raster)
 library(PresenceAbsence)
-source("models/biomod4alps/binarization.R")
+source("data/biomod4alps/binarization.R")
 #####################################
 # loading results and points
 ####################################
 sp<-read.table("endemic_dolo50.txt", sep="\t", h=T) ### load spec occurrence file
 spek <- sub(pattern = "_", replacement = "\\.", x = as.character(unique(sp$species)))
-work_dir <- paste0(getwd(), "/models_future/")
+work_dir <- paste0(getwd(), "/models_topography/")
 out_dir <- paste0(getwd(), "/bin/")  ### cartella dove salva i file, crea cartella bin in wd
 #geo<- raster("geology/geology_endemic_dolo_all.tif")  ### load geology file (1,0)
 
