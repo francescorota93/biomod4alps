c1666769dbd6c8b81169281b5b7ecfe142cd833a
Francesco Rota <francesco.rota@natec.unibz.it>
2020-05-18 11:48:59 +0200

    species 6 7 8 output su /global/lv71284/frota/models_future GLM GBM

--- job_project_future.sh
+++ job_project_future.sh
@@ -14,4 +14,4 @@ module load intel/18 intel-mkl/2018 R/3.6.2 gdal/2.4.1 proj/4.9.3
 
 ## Rscript $HOME/data/biomod4alps/launch_project_future.R all 30
 
-Rscript $HOME/data/biomod4alps/launch_project_future.R -c 25  -w $HOME/data/ --scriptdir $HOME/data/biomod4alps -s 1 2 5  --models  GLM GBM 
+Rscript $HOME/data/biomod4alps/launch_project_future.R -c 25  -w $HOME/data/ --scriptdir $HOME/data/biomod4alps -s 6 7 8  --models  GLM GBM  --outdir $GLOBAL/models_future 
