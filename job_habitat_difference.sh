2dfa26320375c824afec4ecc7e603a7a6cad8e04
Francesco Rota <francesco.rota@natec.unibz.it>
2021-01-15 17:30:04 +0000

    jobs for the divided script

---
+++ job_habitat_difference.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J build
+#SBATCH -N 1
+#SBATCH --partition=mem_0256
+#SBATCH --qos=normal_0256
+##SBATCH --account=lv71284
+##SBATCH --mail-user=giulio.genova@eurac.edu
+#SBATCH --mail-user=francesco.rota@education.unibz.it
+#SBATCH --mail-type=BEGIN,END
+##SBATCH --time-min=3
+
+module purge
+module load intel/18 intel-mkl/2018 R/3.6.2 gdal/2.4.1 proj/4.9.3
+
+## Rscript $HOME/data/biomod4alps/launch_build_model.R all 30
+Rscript $HOME/data/biomod4alps/mean_habitat_difference.R
