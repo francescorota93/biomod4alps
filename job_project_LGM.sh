eaea0389a81bffc7dd58b4be44339360bfcb1b6a
Francesco Rota <francesco.rota@natec.unibz.it>
2021-09-24 17:00:31 +0200

    fix job script

--- job_project_LGM.sh
+++ job_project_LGM.sh
@@ -14,4 +14,4 @@ module load intel/18 intel-mkl/2018 R/3.6.2 gdal/2.4.1 proj/4.9.3
 
 ## Rscript $HOME/data/biomod4alps/launch_project_LGM.R all 30
 
-Rscript $HOME/data/biomod4alps/launch_project_LGM.R -c 20  -w $HOME/data/ --scriptdir $HOME/data/biomod4alps -s 1 5 6 7 --models GLM GBM RF GAM CTA  --temprastdir $GLOBAL/temp_rast_dir       
+Rscript $HOME/data/biomod4alps/launch_project_LGM.R -c 20  -w $HOME/data/ --scriptdir $HOME/data/biomod4alps -s 1 5 6 7 --models GLM GBM RF GAM CTA  --temprastdir $HOME/temp_rast_dir       
