66e6bf4d75f38fe63fc0387f1a6f54bf1bb9552a
Francesco Rota <francesco.rota@natec.unibz.it>
2021-09-28 17:44:04 +0200

    launch mean LGM

---
+++ launch_mean_LGM.R
@@ -0,0 +1,20 @@
+setwd("/home/lv71284/frota/data/") ###setwd("/home/lv71284/frota/data/")
+library(raster)
+library(PresenceAbsence)
+source("biomod4alps/mean_LGM.R")
+#####################################
+# loading results and points
+####################################
+sp<-read.table("endemic_dolo50.txt", sep="\t", h=T) ### load spec occurrence file
+spek <- sub(pattern = "_", replacement = "\\.", x = as.character(unique(sp$species)))
+work_dir <- paste0(getwd(), "/models_LGM/")
+out_dir <- paste0(getwd(), "/mean_LGM/")  ### cartella dove salva i file, crea cartella bin in wd
+### decide if insert glacial maximum rasterized
+#ice<- raster("LGM/.tif")  ### load LGM glaciers extension file (1,0)
+
+# cut on extent per species  t <- table(xmin,ymax) 
+t <- read.table("spec_extents.txt", head = TRUE, sep = "\t")
+
+### made loop for each species
+
+lapply(spek, binarization, sp,  t, work_dir, out_dir)
